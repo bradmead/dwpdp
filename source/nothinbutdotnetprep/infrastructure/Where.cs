539ce6e61c53e1892c3c7ebbcfd67cff77d8defe
Brad Mead <bradmead@gmail.com>
2011-05-16 18:19:08 -0700

    Committing

--- source/nothinbutdotnetprep/infrastructure/Where.cs
+++ source/nothinbutdotnetprep/infrastructure/Where.cs
@@ -1,9 +1,20 @@
+using nothinbutdotnetprep.collections;
+
 namespace nothinbutdotnetprep.infrastructure
 {
   public class Where<Item>
   {
-    public static  something has_a<PropertyType>(PropertyAccessor<Item,PropertyType> accessor)
+    public static ProductionStudio has_a<PropertyType>(PropertyAccessor<Item, PropertyType> accessor)
     {
+      return PropertyEvaluator<Item, PropertyType>.Evaluate(accessor);
+    }
+  }
+
+  public class PropertyEvaluator<Item, PropertyType>
+  {
+    public static ProductionStudio Evaluate(PropertyAccessor<Item, PropertyType> accessor)
+    {
+      return ProductionStudio.Pixar;
     }
   }
 }
