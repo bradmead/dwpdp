827d1d36832e791d9bfc5ba931003bbcd2e2f79f
Brad Mead <bradmead@gmail.com>
2011-05-17 09:33:24 -0700

    Pushing new changes

---
+++ codekata/A_FileSystemDisplayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace codekata
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    //SUT
+    private FileSystemsDisplayer _fileSystemsDisplayer;
+
+    [TestMethod]
+    public void Lists_files_and_folders()
+    {
+      _fileSystemsDisplayer = new FileSystemsDisplayer(null);
+    }
+  }
+
+  public class FileSystemsDisplayer
+  {
+    private readonly ISearchFileSystem _fileSystemSearcher;
+
+    public FileSystemsDisplayer(ISearchFileSystem fileSystemSearcher)
+    {
+      _fileSystemSearcher = fileSystemSearcher;
+    }
+
+
+  }
+
+  public interface ISearchFileSystem
+  {
+    List<string> GetFolders();
+  }
+}
